63eeac91358b1e7a7a4aca8edb3ae6a4bb265f3a
sandeepchan <chandsandeep700@gmail.com>
2019-12-04 20:27:14 +0530

    first

--- src/Components/Rank/Rank.js
+++ src/Components/Rank/Rank.js
@@ -1,18 +1,16 @@
-import React from 'react'
-const Rank= ()=>
-{
-    return(
-        <div>
-            <div className= 'white f3'>
-                {'Sandeep your current rank is...'}
+import React from 'react';
 
-            </div>
-            <div className= 'white f1'>
-                {'#1'}
-
-            </div>
-        </div>
-    )
-
+const Rank = ({ name, entries }) => {
+  return (
+    <div>
+      <div className='white f3'>
+        {`${name}, your current entry count is...`}
+      </div>
+      <div className='white f1'>
+        {entries}
+      </div>
+    </div>
+  );
 }
-export default Rank 
+
+export default Rank;
--- src/Container/App.js
+++ src/Container/App.js
@@ -4,6 +4,8 @@ import Navigation from '../Components/Navigation/Navigation'
 import Logo from '../Components/Logo/Logo'
 import ImageLink from '../Components/ImageLink/ImageLink'
 import Rank from '../Components/Rank/Rank'
+import Signin from '../Components/signin/Signin'
+import Register from '../Components/Register/Register'
 import FaceRecoginition from '../Components/FaceRec/FaceRec'
 import Clarifai from 'clarifai';
 import './App.css';
@@ -32,8 +34,35 @@ class App extends Component
     this.state={
       input: '',
       imageUrl: '', 
-      box: {}
+      box: {},
+      route: 'signin',
+      isSignedIn: false,
+      user : {
+        
+        id: '',
+        name: '',
+        email: '',
+        
+        
+        entries: 0,
+        joined: ''
+      }
     }
+  }
+  // componentDidMount()
+  // {
+  //   fetch('http://localhost:3000')
+  //   .then(response => response.json())
+  //   .then(console.log)
+  // }
+  loadUser = (data) => {
+    this.setState({user: {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+    }})
   }
   calculateFaceLocation =(data) =>
   {
@@ -56,31 +85,73 @@ class App extends Component
     this.setState({input: event.target.value})
 
   }
-  onButtonSubmit=()=>
-  {
-    this.setState({imageUrl: this.state.input})
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(response=> 
-      // do something with response
-      this.displayFaceBox(this.calculateFaceLocation(response)))
-      .catch(err=> console.log(err))
-    
-  
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+    app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL,
+        this.state.input)
+      .then(response => {
+        if (response) {
+          fetch('http://localhost:3000/image', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              id: this.state.user.id
+            })
+          })
+            .then(response => response.json())
+            .then(count => {
+              this.setState(Object.assign(this.state.user, { entries: count}))
+            })
 
+        }
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      })
+      .catch(err => console.log(err));
+  }
+  onRouteChange= (route) =>
+  { if(route=== 'signout')
+    {
+      this.setState({isSignedIn: false})
+    } else if(route === 'home')
+    {
+      this.setState({isSignedIn: true})
+    }
+    this.setState({route:route})
   }
   render() {
+   const { isSignedIn, imageUrl, route, box }= this.state ;
     return(
       <div className="App">
-        <Particles className="particle" params={particle} />
-        <Navigation/>
-        <Logo />
-        <Rank/>
-        <ImageLink onInputChange= {this.onInputChange} onButtonSubmit= {this.onButtonSubmit}/>
         
-        <FaceRecoginition box= {this.state.box} imageUrl= {this.state.imageUrl} />
+        <Particles className="particle" params={particle} />
+        
+        <Navigation isSignedIn= {isSignedIn} onRouteChange= {this.onRouteChange}/>
+        {route === 'home' ?
+       
+        <div>
+          <Logo />
+          
+          <Rank name={this.state.user.name}
+                entries={this.state.user.entries}/> 
+          <ImageLink onInputChange= {this.onInputChange} onButtonSubmit= {this.onButtonSubmit}/>
+          
+          <FaceRecoginition box= {box} imageUrl= {imageUrl} />
+        </div>
+         : (
+          
+            route === 'signin'
+            ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+            : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+           )
+         
+        
+        }
 
       </div>
     )
-  }
+      }
 }
 
 export default App;
